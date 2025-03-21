cf896689c5c9c905c3dcd196fd342341ec18ed2a
otprotee <oscarsmessages@gmail.com>
2025-03-21 15:26:30 +0000

    2

--- questions.java
+++ questions.java
@@ -5,7 +5,7 @@ public class questions {
     static ArrayList<String> basket;
 
     public static void main(String[] args){
-        basket = new ArrayList<String>();
+        basket = new ArrayList<>();
 
         shopping.main(new String[0]);
         int loopBrsch = 0;
@@ -15,6 +15,7 @@ public class questions {
                 System.out.println("Would you like to browse or search for and item: ");
                 String brsrchR =  brsrch.nextLine();
                 String brsrchN =  brsrchR.toLowerCase();
+                Scanner basketS = new Scanner(System.in);
                 try {
                     if (brsrchN.equals("browse") || brsrchN.equals("browse ")) {
                         System.out.println("\n Fruits: \n");
@@ -22,39 +23,46 @@ public class questions {
                         System.out.println("\n Canned Foods: \n");
                         System.out.println(shopping.canned_foods);
                         loopBrsch = 1;
-                    } else if (brsrchN.equals("search") || brsrchN.equals("search ")) {
-                        Scanner srch = new Scanner(System.in);
-                        System.out.println("What do you want to search fruits or canned foods: ");
-                        String item = srch.nextLine();
-                        String itemN = item.toLowerCase();
-                        if (itemN.equals("fruits") || itemN.equals("fruits ")) {
-                            System.out.println("what item do you want to search: ");
-                            String srch1 = srch.nextLine();
-                            String srch1N = srch1.toLowerCase();
-                            for (String range1 : shopping.fruits) {
-                                if (range1.equals(srch1N)) {
-                                    System.out.println(srch1N + " is in the list");
-                                    System.out.println("Do you want to add this to basket yes or no:");
-                                    ////
-
-                                } else {
-                                    System.out.println("We dont sell" + srch1N);
-                                }
-                            }
-                        }
-                        if (itemN.equals("canned foods") || itemN.equals("canned foods ")) {
-                            System.out.println("what item do you want to search: ");
-                            String srch2 = srch.nextLine();
-                            String srch2N = srch2.toLowerCase();
-                            for (String range1 : shopping.fruits) {
-                                if (range1.equals(srch2N)) {
-                                    System.out.println(srch2N + " is in the list");
-                                } else {
-                                    System.out.println("We don't sell" + srch2N);
-                                }
-                            }
-                        }
                     }
+                    else if (brsrchN.equals("search") || brsrchN.equals("search ")) {
+                            Scanner srch = new Scanner(System.in);
+                            System.out.println("What do you want to search fruits or canned foods: ");
+                            String item = srch.nextLine();
+                            String itemN = item.toLowerCase();
+                            if (itemN.equals("fruits") || itemN.equals("fruits ")) {
+                                System.out.println("what item do you want to search: ");
+                                String srch1 = srch.nextLine().toLowerCase();
+                                for (String range1 : shopping.fruits) {
+                                    if (range1.equals(srch1)) {
+                                        System.out.println(srch1 + " is in the list");
+                                        System.out.println("Do you want to add this to basket yes or no:");
+                                        String bsktsrch = basketS.nextLine().toLowerCase();
+                                        if (bsktsrch.equals("yes")) {
+                                            System.out.println("how many do you want to add: ");
+                                            try {
+                                                int bsktnum = basketS.nextInt();
+                                                for (int range2 = 0; range2 < bsktnum; range2++){
+                                                    basket.add(srch1);
+                                                }
+                                            }
+                                            catch(Exception e) {
+                                                System.out.println("Error QM003");
+                                            }
+                                        } else System.out.println("We dont sell " + srch1);
+                                    }
+                                }
+                            }
+                            if (itemN.equals("canned foods") || itemN.equals("canned foods ")) {
+                                System.out.println("what item do you want to search: ");
+                                String srch2 = srch.nextLine().toLowerCase();
+                                for (String range1 : shopping.fruits) {
+                                    if (range1.equals(srch2)) {
+                                        System.out.println("We sell" + srch2);
+                                    }
+                                }
+                                System.out.println("We don't sell " + srch2);
+                            }
+                        }
                 } catch(Exception e) {
                     System.out.println("Error QM002");
                 }
