82e3337b200249cd23f9cc7ec8b0cd246ed68799
otprotee <oscarsmessages@gmail.com>
2025-03-21 15:32:35 +0000

    2

--- questions.java
+++ questions.java
@@ -55,19 +55,32 @@ public class questions {
                             if (itemN.equals("canned foods") || itemN.equals("canned foods ")) {
                                 System.out.println("what item do you want to search: ");
                                 String srch2 = srch.nextLine().toLowerCase();
-                                for (String range1 : shopping.fruits) {
-                                    if (range1.equals(srch2)) {
-                                        System.out.println("We sell" + srch2);
+                                for (String range4 : shopping.fruits) {
+                                    if (range4.equals(srch2)) {
+                                        System.out.println(srch2 + " is in the list");
+                                        System.out.println("Do you want to add this to basket yes or no:");
+                                        String bsktsrch2 = basketS.nextLine().toLowerCase();
+                                        if (bsktsrch2.equals("yes")) {
+                                            System.out.println("how many do you want to add: ");
+                                            try {
+                                                int bsktnum = basketS.nextInt();
+                                                for (int range3 = 0; range3 < bsktnum; range3++){
+                                                    basket.add(srch2);
+                                                }
+                                            }
+                                            catch(Exception e) {
+                                                System.out.println("Error QM004");
+                                            }
+                                        } else System.out.println("We dont sell " + srch2);
                                     }
                                 }
-                                System.out.println("We don't sell " + srch2);
                             }
+                                }
+                        } catch(Exception e) {
+                            System.out.println("Error QM002");
                         }
-                } catch(Exception e) {
-                    System.out.println("Error QM002");
+                    }
                 }
-            }
-        }
         catch(Exception e){
             System.out.println("Error QM001");
         }
