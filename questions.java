7d5bb470efe2d356c3e386f3646950b55017f30f
otprotee <oscarsmessages@gmail.com>
2025-03-21 11:58:42 +0000

    2

--- questions.java
+++ questions.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class questions {
@@ -8,15 +9,28 @@ public class questions {
         try {
             while(loopBrsch == 0) {
                 Scanner brsrch = new Scanner(System.in);
-                System.out.println("Would you like to browse or search for and item?");
+                System.out.println("Would you like to browse or search for and item: ");
                 String brsrchR =  brsrch.nextLine();
-                brsrchR.toLowerCase();
-                if (brsrchR == "browse" || brsrchR == "browse ") {
+                String brsrchN =  brsrchR.toLowerCase();
+                if (brsrchN.equals("browse") || brsrchN.equals("browse ")) {
                     System.out.println("\n Fruits: \n");
                     System.out.println(shopping.fruits);
                     System.out.println("\n Canned Foods: \n");
                     System.out.println(shopping.canned_foods);
+                } else if (brsrchN.equals("search") || brsrchN.equals("search ")) {
+                    Scanner srch = new Scanner(System.in);
+                    System.out.println("What do you want to search fruits or canned foods: ");
+                    String item = srch.nextLine();
+                    String itemN = item.toLowerCase();
+                    if (itemN.equals("fruits") || itemN.equals("fruits ")){
+                        for (int range1 = 0; range1 < fruitLen; range1++) {
+                            
+                        }
+                    }
+
+
                 }
+
 
             }
 
