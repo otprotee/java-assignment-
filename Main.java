7b931e162e35184f7ced6f2cc7fbec70d8c45095
otprotee <oscarsmessages@gmail.com>
2025-03-21 11:09:09 +0000

    2

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@
 public class Main {
     public static void main(String[] args) {
         shopping.main(new String[0]);
-        window.main(new String[0]);
     }
 }
--- questions.java
+++ questions.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class questions {
     public static void main(String[] args){
+        try {
+            Scanner brsrch = new Scanner(System.in);
 
+            
+        }
+        catch(Exception e){
+
+        }
 
     }
 }
